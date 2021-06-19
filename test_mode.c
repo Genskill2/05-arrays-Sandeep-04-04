5dd48af4f38ecde66b1b5ef42b48b6e05a5dc288
Sandeep-04-04 <84789422+Sandeep-04-04@users.noreply.github.com>
2021-06-19 23:37:06 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -12,3 +12,24 @@ int main(void) {
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
   }
+
+
+int mode(int a[],int n) {
+   int maxValue = 0, maxCount = 0, i, j;
+
+   for (i = 0; i < n; ++i) {
+      int count = 0;
+      
+      for (j = 0; j < n; ++j) {
+         if (a[j] == a[i])
+         ++count;
+      }
+      
+      if (count > maxCount) {
+         maxCount = count;
+         maxValue = a[i];
+      }
+   }
+
+   return maxValue;
+}
