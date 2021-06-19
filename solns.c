e9f56692bbe1d1ba73e46129bc034a53d601c4aa
Sandeep-04-04 <84789422+Sandeep-04-04@users.noreply.github.com>
2021-06-19 23:41:28 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -69,3 +69,86 @@ int factors(int n, int arr[]){
        return idx; 
 }
 
+#include <stdio.h>
+#include <assert.h>
+
+
+int max(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (max(x, 7) == 10);
+
+  int y[] = {5};
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
+  }
+  
+int max(int arr[], int size){
+    int max_num = arr[0];
+    for(int i = 0 ; i < size; ++i){
+        if(arr[i] > max_num)
+        max_num = arr[i];
+    }
+    return max_num;
+}
+
+#include <stdio.h>
+#include <assert.h>
+
+
+int min(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (min(x, 7) == -3);
+
+  int y[] = {5};
+  assert (min(y, 1) == 5);
+  printf("Min: passed\n");
+  }
+
+int min(int arr[], int size){
+    int min_num = arr[0];
+    for(int i =0 ; i < size ; ++i){
+        if(arr[i] < min_num)
+        min_num = arr[i];
+    }
+    return min_num;
+}
+
+#include <stdio.h>
+#include <assert.h>
+
+
+int mode(int [], int);
+
+int main(void) {
+  int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
+  assert ((mode(x, 12) == 4));
+
+  int y[] = {5};
+  assert (mode(y, 1) == 5);
+  printf("Mode: passed\n");
+  }
+
+
+int mode(int a[],int n) {
+   int maxValue = 0, maxCount = 0, i, j;
+
+   for (i = 0; i < n; ++i) {
+      int count = 0;
+      
+      for (j = 0; j < n; ++j) {
+         if (a[j] == a[i])
+         ++count;
+      }
+      
+      if (count > maxCount) {
+         maxCount = count;
+         maxValue = a[i];
+      }
+   }
+
+   return maxValue;
+}
