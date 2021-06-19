c60220b1897942a9c21fdad68f571009148cb62a
Sandeep-04-04 <84789422+Sandeep-04-04@users.noreply.github.com>
2021-06-19 23:36:25 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -12,3 +12,12 @@ int main(void) {
   assert (min(y, 1) == 5);
   printf("Min: passed\n");
   }
+
+int min(int arr[], int size){
+    int min_num = arr[0];
+    for(int i =0 ; i < size ; ++i){
+        if(arr[i] < min_num)
+        min_num = arr[i];
+    }
+    return min_num;
+}
